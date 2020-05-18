3971e2d3341ea01b1bb1153c065583409b17d255
fragata-adrian <fragataadrian99@gmail.com>
2020-05-17 22:34:37 -0300

    update README.md add ejercicio 3

--- src/main/java/ar/edu/unahur/obj2/simuladorDeAutos/Main.java
+++ src/main/java/ar/edu/unahur/obj2/simuladorDeAutos/Main.java
@@ -1,4 +1,9 @@
 package ar.edu.unahur.obj2.simuladorDeAutos;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
 }
